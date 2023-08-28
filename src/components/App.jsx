f060352b02ab9868bb13310da219566c2e89c189
RysonShalvis <rysonshalvis@gmail.com>
2023-08-28 00:09:03 -0600

    add react router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,17 @@ import "../scss/_app.scss";
 import "../scss/_reset.scss";
 import Header from "./Header";
 import LandingPage from "./LandingPage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Header />
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<h1>About us</h1>} />
+        <Route path="services" element={<h1>Services</h1>} />
+      </Routes>
     </div>
   );
 }
