a468dd5c3f78401e00bca826f711c75b74f8b991
RysonShalvis <rysonshalvis@gmail.com>
2023-09-29 18:12:55 -0600

    fix apostrophe

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
         <Button path="/services">Learn more</Button>
       </div>
       <Banner className="home-banner">
-        <p>We accept Medicaid&quot;s New Choice Waiver Program waivers.</p>
+        <p>We accept Medicaid&apos;s New Choice Waiver Program waivers.</p>
         <Button buttonColor="secondary-color">Learn More</Button>
       </Banner>
     </div>
