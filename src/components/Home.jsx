ee6bbb800cf7016cf2711816f36d8ea0c489320d
RysonShalvis <rysonshalvis@gmail.com>
2024-08-06 14:34:02 -0600

    wording change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,14 +16,22 @@ const Home = () => {
         <div className="logo-container">
           <img src={logo} />
         </div>
-        <h1>Rosecrest Assisted Living</h1>
-        <h2>&quot;It feels like Home&quot;</h2>
-        <h2>The most affordable assisted living in Saint George</h2>
+        <h1>Rosecrest Assisted & Independent Living</h1>
+        <h2>
+          Because we are small, you receive true personalized care in an actual
+          Home
+        </h2>
+        <h2>Private Rooms starting at $2,000</h2>
         <Button path="/services">Learn more</Button>
       </div>
       <Banner className="home-banner">
-        <p>Accept&apos;s Medicaid&apos;s &quot;New Choice&apos;s Waiver Program&quot;. Contact us to see if you qualify for the waiver</p>
-        <Button buttonColor="secondary-color" path="/Contact">Contact Us</Button>
+        <p>
+          Accept&apos;s Medicaid&apos;s &quot;New Choice&apos;s Waiver
+          Program&quot;. Contact us to see if you qualify for the waiver
+        </p>
+        <Button buttonColor="secondary-color" path="/Contact">
+          Contact Us
+        </Button>
       </Banner>
     </div>
   );
